4ee63bdd50b46559e54e68cf7d19c71de0cfa451
Melissa Perez <perezmelissa053@gmail.com>
2024-07-30 22:57:30 -0400

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="w-full absolute text-white z-10">
       <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
         <Link href={"/"} className="font-bold text-3xl">
-          Cloud Corp
+          Cloud Corp Inc.
         </Link>
         <div className="space-x-4 text-xl">
           <Link href={"/performance"}>Performance</Link>
